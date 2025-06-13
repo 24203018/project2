59d445c3cc24ff00b322790a8a237698bce9d54b
24203018 <24203018@srcas.ac.in>
2025-06-13 12:21:58 +0530

    Create prg3.java

---
+++ prg3.java
@@ -0,0 +1,9 @@
+public class prg3{
+  public static void main(String[] args ){
+    system.out.println("welcome");
+    system.out.println("to");
+    system.out.println("devops");
+    system.out.println("class");
+  }
+}
+
